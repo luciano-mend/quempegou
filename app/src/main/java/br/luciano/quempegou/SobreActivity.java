59aed68c678cceca37ccaa7eac1a6b8a87b19d2f
Luciano Mendonca <luciano.s.mend@gmail.com>
2026-03-08 13:25:22 -0300

    feat: Botão UP tratado via atributo launchMode singleTask na activity principal.

--- app/src/main/java/br/luciano/quempegou/SobreActivity.java
+++ app/src/main/java/br/luciano/quempegou/SobreActivity.java
@@ -19,16 +19,4 @@ public class SobreActivity extends AppCompatActivity {
 
         setTitle(R.string.sobre);
     }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == android.R.id.home) {
-            finish();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
